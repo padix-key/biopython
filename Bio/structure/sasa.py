5ea345c21932364a9d185364c37ec88d7c6e2d4c
Patrick Kunzmann <patrick.kunzm@gmail.com>
2017-06-04 14:27:59 +0200

    Continiued SASA calculation

--- Bio/structure/sasa.py
+++ Bio/structure/sasa.py
@@ -10,13 +10,15 @@ a protein or single atoms.
 
 import numpy as np
 from . import AdjacencyMap
+from . import distance
+from . import vector_dot
 
 
 def sasa(array, **kwargs):
     """
     array : AtomArray
     probe_radius : float
-    relevant_indices : ndarray, optional
+    filter : ndarray(dtype=bool), optional
     ignore_ions : bool
     point_number : int, optional
     point_distr : string or function, optional
@@ -27,13 +29,15 @@ def sasa(array, **kwargs):
     else:
         probe_radius = 1.4
     
-    if "relevant_indices" in kwargs:
-        indices = np.array(kwargs["relevant_indices"])
+    if "atom_filter" in kwargs:
+        atom_filter = np.array(kwargs["filter"]).astype(bool)
     else:
-        indices = np.arange(len(array))
+        atom_filter = np.ones(len(array), dtype=bool)
     
     # Remove water residues, since it is the solvent
-    array = array[array.hetero != "W"]
+    filter = (array.hetero != "W")
+    array = array[filter]
+    atom_filter = atom_filter[filter]
     if "ignore_ions" in kwargs:
         ignore_ions = np.array(kwargs["ignore_ions"])
     else:
@@ -49,7 +53,7 @@ def sasa(array, **kwargs):
         point_distr = kwargs["point_distr"]
     else:
         point_distr = "Fibonacci"
-    if type(point_distr) == function:
+    if callable(point_distr):
         sphere_points = function(point_number)
     elif point_distr == "Fibonacci":
         sphere_points = _create_fibonacci_points(point_number)
@@ -61,36 +65,67 @@ def sasa(array, **kwargs):
         vdw_radii = kwargs["vdw_radii"]
     else:
         vdw_radii = "ProtOr"
-    if instanceof(vdw_radii, ndarray):
+    if isinstance(vdw_radii, np.ndarray):
         radii = vdw_radii
         if len(radii) != len(array):
             raise ValueError("VdW radii array contains insufficient"
                              "amount of elements")
     elif vdw_radii == "ProtOr":
-        if (array.element == "H").any():
-            raise ValueError("ProtOr cannot be used for"
-                             "structures with hydrogen atoms")
-        array = array[array.element != "H"]
+        filter = (array.element != "H")
+        array = array[filter]
+        atom_filter = atom_filter[filter]
         radii = np.zeros(len(array))
         for i in range(len(radii)):
             try:
-                radii[i] = _protor_radii[array.res_name[i]]
-                                        [array.atom_name[i]]
-            except KexError:
+                radii[i] = _protor_radii[array.res_name[i]][array.atom_name[i]]
+            except KeyError:
                 radii[i] = _protor_default
     elif vdw_radii == "Single":
         radii = np.zeros(len(array))
         for i in range(len(radii)):
-            try:
-                radii[i] = _single_radii[array.element[i]]
-            except KexError:
-                radii[i] = _protor_default
+            radii[i] = _single_radii[array.element[i]]
+    # Increase atom radii by probe size ("rolling probe")
     radii += probe_radius
     
     # Box size is as large as the maximum distance, 
     # where two atom can intersect.
     # Therefore intersecting atoms are always in the same or adjacent box.
     adj_map = AdjacencyMap(array, np.max(radii)*2)
+    
+    # Only calculate SASA for relevant atoms
+    area_per_point = 4.0 * np.pi / point_number
+    sasa = np.full(len(array),np.nan)
+    for index in np.arange(len(array))[atom_filter]:
+        coord = array.coord[index]
+        radius = radii[index]
+        # Transform the sphere dots to the current atom
+        sphere_points_transformed = sphere_points * radius + coord
+        # Get coordinates of adjacent atoms
+        adj_indices = adj_map.get_atoms_in_box(coord)
+        adj_radii = radii[adj_indices]
+        adj_atom_coord = array.coord[adj_indices]
+        # Remove all atoms, where the distance to the relevant atom
+        # is larger than the sum of the radii,
+        # since those atoms do not touch
+        # If distance is 0, it is the same atom,
+        # and the atom is removed from the list as well
+        dist = distance(adj_atom_coord, coord)
+        dist_filter = ((dist < adj_radii+radius) & (dist != 0))
+        adj_atom_coord = adj_atom_coord[dist_filter]
+        adj_radii = adj_radii[dist_filter]
+        # Calculate distances between sphere points and adjacent atoms
+        diff = (adj_atom_coord[np.newaxis, :, :]
+                - sphere_points_transformed[:, np.newaxis, :])
+        sq_distance = vector_dot(diff, diff)
+        # Calculate the difference between the adjacent atom radius
+        # and the distance from the atom to the sphere points
+        # If the value for all atoms is larger than 0,
+        # the point is solvent accessible
+        sq_radius_distance = sq_distance - adj_radii*adj_radii
+        min_sq_radius_distance = np.min(sq_radius_distance, axis=1)
+        accessible_point_count = np.count_nonzero(min_sq_radius_distance > 0)
+        sasa[index] = area_per_point * accessible_point_count * radius*radius
+    return sasa
 
 
 def _create_fibonacci_points(n):
@@ -109,202 +144,182 @@ def _create_fibonacci_points(n):
 
 
 _protor_default = 1.80
-C3H0s = 1.61
-C3H0b = 1.61
-C4H1s = 1.88
-C4H1b = 1.88
-C3H1s = 1.76
-C3H1b = 1.76
-C3H2u = 1.76
-C4H2s = 1.88
-C4H2b = 1.88
-C4H3u = 1.88
-N3H0u = 1.64
-N3H1s = 1.64
-N3H1b = 1.64
-N4H3u = 1.64
-N3H2u = 1.64
-O1H0u = 1.42
-O2H1u = 1.46
-O2H2u = 1.46
-S2H0u = 1.77
-S2H1u = 1.77
-_protor_radii = {"GLY": {" O  ": O1H0u,
-                         " C  ": C3H0b,
-                         " CA ": C4H2s,
-                         " N  ": N3H1s},
-                 "ALA": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1b,
-                         " N  ": N3H1s,
-                         " CB ": C4H3u},
-                 "VAL": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H1b,
-                         " CG1": C4H3u,
-                         " CG2": C4H3u},
-                 "LEU": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H2s,
-                         " CG ": C4H1b,
-                         " CD1": C4H3u,
-                         " CD2": C4H3u},
-                 "ILE": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H1b,
-                         " CG1": C4H2b,
-                         " CG2": C4H3u,
-                         " CD1": C4H3u},
-                 "PRO": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1b,
-                         " N  ": N3H0u,
-                         " CB ": C4H2b,
-                         " CG ": C4H2b,
-                         " CD ": C4H2s},
-                 "MET": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H2s,
-                         " CG ": C4H2b,
-                         " SD ": S2H0u,
-                         " CE ": C4H3u},
-                 "PHE": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H2s,
-                         " CG ": C3H0b,
-                         " CD1": C3H1s,
-                         " CD2": C3H1b,
-                         " CE1": C3H1b,
-                         " CE2": C3H1b,
-                         " CZ ": C3H1b},
-                 "TYR": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H2s,
-                         " CG ": C3H0b,
-                         " CD1": C3H1s,
-                         " CD2": C3H1s,
-                         " CE1": C3H1s,
-                         " CE2": C3H1s,
-                         " CZ ": C3H0b,
-                         " OH ": O2H1u},
-                 "TRP": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H2s,
-                         " CG ": C3H0b,
-                         " CD1": C3H1s,
-                         " NE1": N3H1b,
-                         " CD2": C3H0b,
-                         " CE2": C3H0b,
-                         " CE3": C3H1b,
-                         " CZ3": C3H1b,
-                         " CZ2": C3H1b,
-                         " CH2": C3H1b,
-                         "CEH2": C3H1b},
-                 "SER": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H2s,
-                         " OG ": O2H1u,
-                         " OG1": O2H1u},
-                 "THR": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H1b,
-                         " OG1": O2H1u,
-                         " CG2": C4H3u,
-                         " CG ": C4H3u},
-                 "ASN": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H2s,
-                         " CG ": C3H0b,
-                         " OD1": O1H0u,
-                         " ND2": N3H2u},
-                 "GLN": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H2s,
-                         " CG ": C4H2s,
-                         " CD ": C3H0b,
-                         " OE1": O1H0u,
-                         " NE2": N3H2u},
-                 "CYS": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H2b,
-                         " SG ": S2H1u},
-                 "CSS": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H2b,
-                         " SG ": S2H0u},
-                 "HIS": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H2s,
-                         " CG ": C3H0b,
-                         " ND1": N3H1b,
-                         " CD2": C3H1s,
-                         " CE1": C3H1s,
-                         " NE2": N3H1b},
-                 "GLU": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H2s,
-                         " CG ": C4H2s,
-                         " CD ": C3H0b,
-                         " OE1": O1H0u,
-                         " OE2": O1H0u},
-                 "ASP": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H2s,
-                         " CG ": C3H0b,
-                         " OD1": O1H0u,
-                         " OD2": O1H0u},
-                 "ARG": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H2s,
-                         " CG ": C4H2s,
-                         " CD ": C4H2s,
-                         " NE ": N3H1b,
-                         " CZ ": C3H0b,
-                         " NH1": N3H2u,
-                         " NH2": N3H2u},
-                 "LYS": {" O  ": O1H0u,
-                         " C  ": C3H0s,
-                         " CA ": C4H1s,
-                         " N  ": N3H1s,
-                         " CB ": C4H2s,
-                         " CG ": C4H2s,
-                         " CD ": C4H2s,
-                         " CE ": C4H2b,
-                         " NZ ": N4H3u}}
+_protor_radii = {"GLY": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64},
+                 "ALA": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88},
+                 "VAL": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " CG1": 1.88,
+                         " CG2": 1.88},
+                 "LEU": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " CG ": 1.88,
+                         " CD1": 1.88,
+                         " CD2": 1.88},
+                 "ILE": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " CG1": 1.88,
+                         " CG2": 1.88,
+                         " CD1": 1.88},
+                 "PRO": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " CG ": 1.88,
+                         " CD ": 1.88},
+                 "MET": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " CG ": 1.88,
+                         " SD ": 1.77,
+                         " CE ": 1.88},
+                 "PHE": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " CG ": 1.61,
+                         " CD1": 1.76,
+                         " CD2": 1.76,
+                         " CE1": 1.76,
+                         " CE2": 1.76,
+                         " CZ ": 1.76},
+                 "TYR": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " CG ": 1.61,
+                         " CD1": 1.76,
+                         " CD2": 1.76,
+                         " CE1": 1.76,
+                         " CE2": 1.76,
+                         " CZ ": 1.61,
+                         " OH ": 1.46},
+                 "TRP": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " CG ": 1.61,
+                         " CD1": 1.76,
+                         " NE1": 1.64,
+                         " CD2": 1.61,
+                         " CE2": 1.61,
+                         " CE3": 1.76,
+                         " CZ3": 1.76,
+                         " CZ2": 1.76,
+                         " CH2": 1.76,
+                         "CEH2": 1.76},
+                 "SER": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " OG ": 1.46,
+                         " OG1": 1.46},
+                 "THR": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " OG1": 1.46,
+                         " CG2": 1.88,
+                         " CG ": 1.88},
+                 "ASN": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " CG ": 1.61,
+                         " OD1": 1.42,
+                         " ND2": 1.64},
+                 "GLN": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " CG ": 1.88,
+                         " CD ": 1.61,
+                         " OE1": 1.42,
+                         " NE2": 1.64},
+                 "CYS": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " SG ": 1.77},
+                 "CSS": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " SG ": 1.77},
+                 "HIS": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " CG ": 1.61,
+                         " ND1": 1.64,
+                         " CD2": 1.76,
+                         " CE1": 1.76,
+                         " NE2": 1.64},
+                 "GLU": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " CG ": 1.88,
+                         " CD ": 1.61,
+                         " OE1": 1.42,
+                         " OE2": 1.42},
+                 "ASP": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " CG ": 1.61,
+                         " OD1": 1.42,
+                         " OD2": 1.42},
+                 "ARG": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " CG ": 1.88,
+                         " CD ": 1.88,
+                         " NE ": 1.64,
+                         " CZ ": 1.61,
+                         " NH1": 1.64,
+                         " NH2": 1.64},
+                 "LYS": {" O  ": 1.42,
+                         " C  ": 1.61,
+                         " CA ": 1.88,
+                         " N  ": 1.64,
+                         " CB ": 1.88,
+                         " CG ": 1.88,
+                         " CD ": 1.88,
+                         " CE ": 1.88,
+                         " NZ ": 1.64}}
 
 
 _single_radii = {"H":  1.20,
